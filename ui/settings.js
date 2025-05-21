86c817b59f8f4c8e332e69f27529b43002db46e4
HillDev <hillmusic007@gmail.com>
2025-05-21 01:11:48 -0300

    Pushing to github to continue work.

--- ui/settings.js
+++ ui/settings.js
@@ -1,50 +1,48 @@
-function addEntry(list_table, name) {
+function rebuildTable(list_table, set) {
 
-	/* Creating the new entry. */
-	let new_entry = list_table.insertRow()
-	let site = new_entry.insertCell()
-	let button = new_entry.insertCell()
-	let button_element = document.createElement("input")
-	site.textContent = name
-	new_entry.id = site.textContent
+	// Clearing the table.
+	for (let i = list_table.rows.length - 1; i > 0; i--) {
+		list_table.deleteRow(i)
+	}
 
-	button_element.setAttribute("type", "button")
-	button_element.setAttribute("value", "-")
-	button_element.setAttribute("class", "opt_button")
-	button_element.setAttribute("ref", name)
+	/* Iterating over the set, creating a new entry after each one. */
+	set.forEach(entry => {
 
-	/* Adding it to the list. */
-	button.appendChild(button_element)
-	button_element.addEventListener("click", function (e) {
-		document.getElementById(this.getAttribute("ref")).remove()
-	})
+		let new_entry = list_table.insertRow()
+		let site = new_entry.insertCell()
+		let options = new_entry.insertCell()
+		let button_element = document.createElement("input")
+
+		site.textContent = entry
+		new_entry.id = entry
+
+		button_element.setAttribute("type", "button")
+		button_element.setAttribute("value", "-")
+		button_element.setAttribute("class", "opt_button")
+		button_element.setAttribute("ref", entry)
+
+		/* Adding it to the list. */
+		options.appendChild(button_element)
+		button_element.addEventListener("click", function (e) {
+			document.getElementById(this.getAttribute("ref")).remove()
+		})
+	});
 
 }
 
-function saveSettings() {
-	console.log("TODO: saveSettings Implementation")
-}
-
-async function loadSettings() {
-	console.log("TODO: loadSettings Implementation")
-}
-
-async function loadSiteList() {
-	console.log("TODO: loadSettings Implementation")
-}
 
 // Entry point
 async function main() {
-	const settings = await loadSettings()
+	// 
+	let { _sites } = await chrome.storage.local.get("sites")
+	let sites = new Set(_sites)
+	console.log(sites)
 
-	// Important UI Stuff
 	// table that shows the site list
 	const tableref = document.getElementById("list_area").getElementsByTagName('tbody')[0]
-	// Addsite button.
-	const button = document.getElementById("addsite-button");
+	const button = document.getElementById("addsite-button")
 
 	button.addEventListener("click", function (e) {
-
 		const sitehostname = document.getElementById("addsite-text").value
 
 		// Input sanitizing
@@ -52,22 +50,11 @@ async function main() {
 			return
 		}
 
-		// Checking if the element already exists.
+		// This will naturally check if the table already contains this element.
+		sites.add(sitehostname)
 
-		// TODO: Change implementation, use a Set() and pass that to addEntry(), 
-		// then rebuild the whole table each time that function is called.
-		for (let i = 0; i < tableref.rows.length; i++) {
-			let cur_row = tableref.rows[i];
-			console.log(cur_row.textContent)
-			if (cur_row.textContent == sitehostname) {
-				return
-			}
-		}
-
-
-
-		/* Creating new table element. */
-		addEntry(tableref, sitehostname)
+		/* Rebuilding the table */
+		rebuildTable(tableref, sites)
 
 	})
 }
