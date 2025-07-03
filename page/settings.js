e9136be7f6fd512cff67b1d9eae8fb841c198547
HillDev <hillmusic007@gmail.com>
2025-07-03 17:05:58 -0300

    First version of the page sitelist, revisit later

--- page/settings.js
+++ page/settings.js
@@ -3,35 +3,35 @@
 // Rebuilds the site list so it matches the set. Trigger this every time the addsite button is pressed.
 //¿reg
 function rebuildTable(site_container, set) {
-	
+
 	// Clearing the table.
 	for (let i = list_table.rows.length - 1; i > 0; i--) {
 		list_table.deleteRow(i)
 	}
-	
+
 	/* Iterating over the set, creating a new entry after each one. */
 	set.forEach((entry) => {
-		
+
 		// Creating the entry
 		let new_entry = list_table.insertRow()
 		let site = new_entry.insertCell()
 		let options = new_entry.insertCell()
 		let button_element = document.createElement("input")
-		
+
 		site.textContent = entry
 		new_entry.id = entry
-		
+
 		// Affecting the button_element
 		button_element.setAttribute("type", "button")
 		button_element.setAttribute("value", "-")
 		button_element.setAttribute("class", "opt_button")
 		button_element.setAttribute("ref", entry)
-		
+
 		/* Adding it to the list. */
 		options.appendChild(button_element)
-		
+
 		// Making the delete button do stuff
-		button_element.addEventListener("click", function () {
+		button_element.addEventListener("click", function() {
 			document.getElementById(this.getAttribute("ref")).remove()
 			set.delete(this.getAttribute("ref"))
 		})
@@ -45,37 +45,39 @@ function rebuildTable(site_container, set) {
 function handleAnimationSwitch() {
 	document.body.classList = ""
 	let _current_section = document.getElementsByClassName("content-show")[0]
-	
+
 	_current_section.classList.remove("content-show")
 	_current_section.classList.add("content-hide")
-	
+
 	document.getElementById(this.getAttribute("ref")).classList.add("content-show")
-	document.getElementById(this.getAttribute("ref")).classList.remove("content-hide")	
-	
+	document.getElementById(this.getAttribute("ref")).classList.remove("content-hide")
+
 }
 //?reg
 
 // Entry point
 //¿reg
 async function main() {
-	
+
 	/* Making the navigation bar functional */
 	const nav_buttons = document.getElementsByClassName("navbar-inv-radio")
 	Array.from(nav_buttons)
-		.forEach( (el) => {
+		.forEach((el) => {
 			el.addEventListener("click", handleAnimationSwitch)
 		})
-	
+
 	// Get the site list container.
 	const sitelist_container = document.getElementById("sitelist-container")
-	
+	const sitelist_placeholder = document.getElementById("sitelist-placeholder-container")
+
 	// If the list has no elements spawn the placeholder.
-	if (sitelist_container.childElementCount <= 0) {
-		console.log("Spawn placeholder.")
-	}	
-	
-	
-	
+	if (!sitelist_container.childElementCount <= 0) {
+		console.log("Removing placeholder.")
+		sitelist_placeholder.remove()
+	}
+
+
+
 }
 //?reg
 
