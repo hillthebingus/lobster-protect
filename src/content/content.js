89452743a92cc716d4b2c1ab1c84977fc80a01ab
HillDev <hillmusic007@gmail.com>
2025-07-22 17:50:06 -0300

    Finished the service worker and content scripts
    Until i decide to add more functionality

--- src/content/content.js
+++ src/content/content.js
@@ -6,16 +6,16 @@ function Log(is_enabled, fn, ...msg) {
 }
 // ?reg
 
-/* TODO:
- * Overrides & working with new sitels format.
- *
- * Find a way to implement these features.
- */
 // Called only when LobsterProtect is triggered ¿reg
 function onTrigger(dbg, order, _override) {
-	Log(dbg, "onTrigger", "Triggered");
+	Log(dbg, "onTrigger", "Triggered.");
 
-	switch (order.action) {
+
+	// Use the override if we have one.
+	const action = (_override === null) ? order.action : _override.action
+
+
+	switch (action) {
 		case E_Actions.CLOSE_WINDOW:
 			Log(dbg, "onTrigger::CLOSE_WINDOW", "Connecting to service worker...");
 			const _orders = chrome.runtime.connect({ name: "orders" });
@@ -46,16 +46,17 @@ async function main() {
 	Log(true, "main", "Welcome to LobsterProtect!");
 
 	// Getting the response object
+	// TODO Once the options page is finished, remove the defaults.
 	const { sitels, opts } = await chrome.storage.local.get({
 		sitels: [
 			{
 				url: "www.youtube.com",
-				strictness: -1,
+				leniency: 0,
 				action_override: null
 			},
 			{
 				url: "www.google.com",
-				strictness: -1,
+				leniency: 0,
 				action_override: null
 			}
 		],
@@ -69,22 +70,17 @@ async function main() {
 		},
 	});
 
-
-	// Building a set out of the sites array, to prevent duplicate entries if any.
-	const siteset = new Set(sitels);
-	Log(opts.debug, "main", "Loaded site list:", siteset);
-
 	// Getting the current site
 	const current_site = window.location.hostname;
 	Log(opts.debug, "main", "Found hostname:", current_site);
 
 	// If we're on a site we shouldn't be in
-	// FIXME this doesn't work anymore.
-	if (siteset.has(current_site) && !opts.whitelist) {
-		onTrigger(opts.debug, opts.on_trigger);
-	} else if (!siteset.has(current_site) && opts.whitelist) {
-		onTrigger(opts.debug, opts.on_trigger);
-	}
+	sitels.forEach(e => {
+		// Unreadable magic, but it just checks our current site keeping in mind our whitelist.
+		if (e.url === current_site && opts.whitelist === false || e.url !== current_site && opts.whitelist === true) {
+			onTrigger(opts.debug, opts.on_trigger, e.action_override)
+		}
+	});
 }
 
 // Loading the entry point after content loads.
--- src/content/utils.js
+++ src/content/utils.js
@@ -13,29 +13,13 @@ class SiteLSEntry {
 	/**
 	 * Returns a sitelist entry object.
 	 * @param _hostname {string}
-	 * @param _strictness {number}
+	 * @param _leniency {number}
 	 * @param _action_override {SiteLSActionOverride | null}
 	 *
 	 */
-	constructor(_hostname, _strictness, _action_override) {
-		// i fricking hate javascript
-		// Typechecking ¿reg
-		if (typeof _hostname != "string") {
-			return null;
-		}
-
-		if (typeof _hostname != "number") {
-			return null;
-		}
-
-		if (typeof _action != "object") {
-			return null;
-		}
-		// ?reg
-
-		this.hostname = _hostname;
-		this.strictness = _strictness;
-
+	constructor(_hostname, _leniency, _action_override) {
+		this.hostname = (typeof _hostname === "string") ? _hostname : null;
+		this.leniency = (typeof _leniency === "number") ? _leniency : null;
 		// This should be an object detailing what the actions for this object are
 		// Use new SiteLSActionOverride(args), don't type this object manually
 		this.action_override = _action_override;
@@ -48,41 +32,15 @@ class SiteLSEntry {
  * @class SiteLSActionOverride Represents the personal override for an entry on the sitelist.
  */
 class SiteLSActionOverride {
-
 	/**
-	 * @param _redirect_url {string}
-	 * @param _close_win {boolean}
-	 * @param _alert_tolerance {boolean}
+	 * @param _action_override {Object}
 	 */
-	constructor(_redirect_url, _close_win, _alert_tolerance) {
-		// NULL indicates that that option should be as global settings
-		// FIXME This looks awful
-		this.redirect = null
-		this.redirect_url = null
-		this.close_win = null
-		this.alert_tolerance = null
-
-		/* TODO
-		 * Refartdoor
-		 *
-		 * This code is straight out of yandev
-		 * Maybe a forEach loop?
-		 * arguments object?
-		 */
-		// Yandev looking code ¿reg
-		if (_redirect_url != null) {
-			this.redirect = true;
-			this.redirect_url = _redirect_url;
+	constructor(_action_override) {
+		if (!("action" in _action_override && "redirect" in _action_override)) {
+			console.error("Invalid action_override object.");
+			return null;
 		}
-
-		if (_close_win != null) {
-			this.close_win = _close_win;
-		}
-
-		if (_alert_tolerance != null) {
-			this.alert_tolerance = _alert_tolerance;
-		}
-		// ?reg
+		this.action_override = _action_override ? _action_override : null;
 	}
 }
 // ?reg
